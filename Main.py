902bc70d83f2554a660d1d936d1bd04e6704651b
ThatOnlyWeb <95704278+ThatOnlyWeb@users.noreply.github.com>
2022-02-09 14:42:54 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import random
 Numbs = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-names = ('bob', 'jeff', 'sus')
+names = ('bob', 'jeff', 'dylan', 'table')
 emails = ('@gmail.com', 'outook.com', '@yandex.com')
 Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def print_names():
@@ -20,10 +20,12 @@ def print_pass():
 def gen_pass():
     print(str(random.choice(Letters)))
     print(str(random.choice(Numbs)))
+def prnt_line():
+    print("----------------")
 
 print_usr()
-print_names()
-print_names()
+for b in range(2):
+    print_names()
 print_numbs()
 print_numbs()
 print_emails()
@@ -31,5 +33,6 @@ print_pass()
 gen_pass()
 gen_pass()
 gen_pass()
-
+for x in range(2):
+    prnt_line()
 
